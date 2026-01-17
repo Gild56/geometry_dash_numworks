f2c145267b54a0a57bf8901589efce8a79d372f6
Gild56 <gild56gmd@gmail.com>
2026-01-17 19:10:38 +0100

    fix comment

--- gd.py
+++ gd.py
@@ -1,5 +1,5 @@
 # A recreation of Geometry Dash for Numworks calculators. Probably won't work after the Numworks update
-## Credits: Calm_Repeat_7267 and wperez274
+# Credits: Calm_Repeat_7267 and wperez274
 
 from time import sleep
 from random import randint
